5d2fe5875e3d24e51011acff9c88daefe5c13791
Lizzie Chan <lchan217@gmail.com>
2019-02-21 18:45:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,19 @@
 class Dog 
-  @@all = []
-  attr_accessor :name 
+  attr_reader :name
+  @@all = [] 
+  
   def initialize(name)
     @name = name 
     @@all << self 
   end 
-  def self.all
-    @@all.each do |name|
-      puts name.name
-    end 
-  end 
+  
   def self.clear_all 
     @@all.clear 
+  end 
+  
+  def self.all 
+    @@all.each do |dog|
+      puts dog.name
+    end
   end 
 end 
